1e580b2ca981f7907a768ddf30146e4df2d8f8e8
AbsintheSung <h90243768@gmail>
2024-05-29 16:51:05 +0800

    [初始化]-專案初始化

---
+++ js/test.js
@@ -0,0 +1,15 @@
+const testBtn = document.querySelector('.test_btn')
+const testBtn2 = document.querySelector('.test_btn2')
+const testBtn3 = document.querySelector('.test_btn3')
+
+testBtn.addEventListener('click', function () {
+    location.href = "./index.html"
+})
+
+testBtn2.addEventListener('click', function () {
+    location.href = "./contact.html"
+})
+
+testBtn3.addEventListener('click', function () {
+    location.href = "./about.html"
+})
