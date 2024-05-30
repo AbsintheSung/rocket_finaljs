89bea29b77050e16f6f0c7d00ad3c7a4ae5c8c4a
AbsintheSung <h90243768@gmail.com>
2024-05-31 01:42:31 +0800

    feat:添加假資料，資料已api回傳為主

---
+++ js/backend.js
@@ -0,0 +1,56 @@
+//假資料
+const data = {
+    "status": true,
+    "orders": [
+        {
+            "createdAt": 1716275072,
+            "total": 51000,
+            "quantity": 4,
+            "id": "0GKApY4YcHlBCg2HijfI",
+            "user": {
+                "address": "12123123",
+                "name": "ssss",
+                "tel": "0932051549",
+                "payment": "ATM",
+                "email": "h90243768@gmail.com"
+            },
+            "products": [
+                {
+                    "images": "https://hexschool-api.s3.us-west-2.amazonaws.com/custom/tTQQggBAfhl0vEeM7WmTwij1JbAwC7MBD2TSyjHbZHvBjnLU5tOtoGszOmODOEu6DRjzdjIpcEzeCCkmRWaL8JDJwaaWglER7P3V4JXKT7FT5gm0UL45CaHpSVlEXbKK.png",
+                    "quantity": 2,
+                    "price": 15000,
+                    "description": "Charles 雙人床架有著多種異材組合，北美黃楊木打造的床架，仿真皮革的床頭靠枕再加上烤漆金屬的床腳，體現實木床架Mid-Century Modern(世紀中期現代風)美學；且床腳筆直設計，床底寬敞空間考量現代人收納儲物需求，推薦給尋覓復古風雙人床架的你，同款式另有雙人標準床架(5*6.2)尺寸。",
+                    "origin_price": 17000,
+                    "id": "3STbcpyj9Qqt9KaSitfr",
+                    "title": "Charles 雙人床架",
+                    "category": "床架"
+                },
+                {
+                    "images": "https://hexschool-api.s3.us-west-2.amazonaws.com/custom/Zr4h1Oqvc6NtAnpe5pNqJfGYyBJshAlKctfv0BTAZBqVAuvfSAWk4bcidBd8qBu1lRn5TWvib6O3UbmIAEt5w8SdB94GuFplZn6IM4SBvtxWJA2VnOqvQOsKungCPDXv.png",
+                    "quantity": 1,
+                    "price": 9000,
+                    "description": "喬丹6尺雙人加大床頭片",
+                    "origin_price": 12000,
+                    "id": "3lSb3g5Z8RqhjldnL8hp",
+                    "title": "Jordan 雙人床架／雙人加大",
+                    "category": "床架"
+                },
+                {
+                    "images": "https://hexschool-api.s3.us-west-2.amazonaws.com/custom/VYPOpg83Jg7Ft5raI0eyzs6xSz0ttKzSJSklFq7il0HdwJ1iQ9kLHAU19rmkSapmyzRErTm1sxWO725W7CnowkRjwxgSyyRBo1ihGLwNbfYtgbo2nCTY9sarjqoxQbYX.png",
+                    "quantity": 1,
+                    "price": 12000,
+                    "description": "Antony 雙人床架加大選用北美黃楊木打造，木質堅韌且紋理細膩，胡桃木色手工塗裝持久不掉色，彎角導圓、造型簡約、黃銅妝點，體現床架 Mid-Century Modern (世紀中期現代風家具)美學，推薦給喜愛復古風雙人床架的你。同款式還有雙人床架(5*6.2)尺寸，適合寢室空間有限的你。",
+                    "origin_price": 18000,
+                    "id": "Qh0dTcA8zu6YVBtJTH7m",
+                    "title": "Antony 雙人床架／雙人加大",
+                    "category": "床架"
+                }
+            ],
+            "paid": true,
+            "updatedAt": 1716275083
+        }
+    ]
+}
+
+
+
