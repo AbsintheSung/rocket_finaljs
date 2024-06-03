85cc208fe0195680187db6faf81d493a22a0b889
misu13 <djmisu13@gmail.com>
2024-06-03 17:03:05 +0800

    update-修正頁面渲染函式與篩選功能

--- js/index.js
+++ js/index.js
@@ -3,7 +3,11 @@ const apiPath = 'yixuan';
 const productListUrl = `https://livejs-api.hexschool.io/api/livejs/v1/customer/${apiPath}/products`;
 const cartUrl = `https://livejs-api.hexschool.io/api/livejs/v1/customer/${apiPath}/carts`;
 
+
+
 //網頁元素們
+const productCategory = document.querySelector('.product-category');
+
 const productList = document.querySelector('.product-list'); 
 const productImg = document.querySelector('.product-img');
 const addProduct = document.querySelector('.add-product');
@@ -15,38 +19,56 @@ const salePrice = document.querySelector('.sale-price');
 
 
 
-
+let productsData = [];
 
 //取得產品列表
 const getProductList = () => {
     axios.get(productListUrl)
         .then(response => {
-            console.log(response.data.products)
-            let allProduct = '';
-            response.data.products.forEach(obj => {
-                allProduct +=
-                    `<li class="product-cards" data-category="${obj.category}">
-                    <span class="product-type">新品</span> 
-                    <img class="product-img" src="${obj.images}">
-                    <a href="#" alt="加入購物車" class="add-product" data-id="${obj.id}">加入購物車</a>
-                    <h3 class="product-name">${obj.title}</h3>
-                    <del class="original-price">NT$${obj.origin_price}</del>
-                    <p class="sale-price">NT$${obj.price}</p>
-                    </li>`;
-            });
-            renderHtml(productList, allProduct);
+            productsData = response.data.products;
+            renderProducts(productsData);
         })
         .catch(error => {
             console.log(error.response);
         });
 }
 
-getProductList();
+const renderProducts = (products) =>{
+    let allProduct = '';
+    products.forEach(obj => {
+        allProduct +=
+            `<li class="product-cards" data-category="${obj.category}">
+            <span class="product-type">新品</span> 
+            <img class="product-img" src="${obj.images}">
+            <a href="#" alt="加入購物車" class="add-product" data-id="${obj.id}">加入購物車</a>
+            <h3 class="product-name">${obj.title}</h3>
+            <del class="original-price">NT$${obj.origin_price}</del>
+            <p class="sale-price">NT$${obj.price}</p>
+            </li>`;
+    });
+    renderHtml(productList,allProduct);
+}
+
+
 
 //渲染物件到頁面上
 const renderHtml = ( element , data ) =>{
     element.innerHTML = data;
 }
+
+//篩選資料
+const checkProduct = (event) => {
+    let selectedCategory = event.target.value;
+    let filteredProducts = productsData.filter(item => {
+        return selectedCategory === "全部" || item.category === selectedCategory;
+    });
+    renderProducts(filteredProducts);
+}
+
+getProductList();
+
+//監聽篩選選單
+productCategory.addEventListener('change',checkProduct);
 
 const obj =  {
     "data": {
